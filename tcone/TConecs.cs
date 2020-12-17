dfec03754aed3638422809272db285c5a5d6dfc0
tshevka <katekuzmenko.kk13@gmail.com>
2020-12-17 08:10:15 +0200

    Добавьте файлы проекта.

---
+++ tcone/TConecs.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tcone
+{
+    class TCone : TCircle
+    {
+        private double height;
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException(nameof(value));
+                else this.height = value;
+            }
+        }
+
+        public TCone() : base()
+        {
+            Height = 1;
+        }
+
+        public TCone(double height)
+        {
+            this.height = height;
+        }
+
+        public TCone(double radius, double height) : base(radius) => this.Height = height;
+
+        public override string ToString() => base.ToString() + $"\tHeight:{this.Height}";
+
+        public TCone(TCircle circle, double height) : base(circle)
+        {
+            this.Height = height;
+        }
+
+        public double Volume()
+        {
+            return (1 / 3) * Math.PI * Math.Pow(Radius, 2) * Height;
+        }
+        public static bool operator >(TCone cone1, TCone cone2)
+        {
+            return cone1.Volume() > cone2.Volume();
+        }
+
+        public static bool operator <(TCone cone1, TCone cone2)
+        {
+            return cone1.Volume() < cone2.Volume();
+        }
+
+        public static bool operator ==(TCone cone1, TCone cone2)
+        {
+            return cone1.Volume() == cone2.Volume();
+        }
+
+        public static bool operator !=(TCone cone1, TCone cone2)
+        {
+            return cone1.Volume() != cone2.Volume();
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            if (this.GetType() != obj.GetType()) return false;
+            TCone temp = (TCone)obj;
+            return Volume() == temp.Volume();
+        }
+
+        public override int GetHashCode()
+        {
+            return Volume().GetHashCode();
+        }
+    }
+}
---
+++ tcone/Tcircle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tcone
+{
+    public class TCircle
+    {
+        private double radius;
+
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                if (value <= 0) Console.Write("The input isn't correct");
+                else this.radius = value;
+            }
+        }
+
+        public TCircle() => this.Radius = 1;
+        public TCircle(double radius) => this.Radius = radius;
+        public TCircle(TCircle circle) => this.Radius = circle.Radius;
+
+        public override string ToString() { return $"Radius:{this.Radius}"; }
+
+        public double GetLength() { return 2 * Math.PI * Radius; }
+
+        public double GetSquare() { return Math.PI * Math.Pow(Radius, 2); }
+
+
+        public static bool operator >(TCircle circle1, TCircle circle2) { return circle1.Radius > circle2.Radius; }
+        public static bool operator <(TCircle circle1, TCircle circle2) { return circle1.Radius < circle2.Radius; }
+        public static bool operator ==(TCircle circle1, TCircle circle2) { return circle1.Radius == circle2.Radius; }
+        public static bool operator !=(TCircle circle1, TCircle circle2) { return circle1.Radius != circle2.Radius; }
+
+        public static TCircle operator +(TCircle circle1, TCircle circle2)
+        {
+            return new TCircle(circle1.Radius + circle2.Radius);
+        }
+
+        public static TCircle operator -(TCircle circle1, TCircle circle2)
+        {
+            return new TCircle(Math.Abs(circle1.Radius - circle2.Radius));
+        }
+
+        public static TCircle operator *(TCircle circle1, double number)
+        {
+            return new TCircle(circle1.Radius * number);
+        }
+
+        public static TCircle operator *(double number, TCircle circle1)
+        {
+            return new TCircle(circle1.Radius * number);
+        }
+
+        public double GetSector(double angle)
+        {
+            return Math.PI * Math.Pow(this.Radius, 2) * (angle / 360);
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            if (this.GetType() != obj.GetType()) return false;
+            TCircle temp = (TCircle)obj;
+            return Radius == temp.Radius;
+        }
+
+        public override int GetHashCode()
+        {
+            return Radius.GetHashCode();
+        }
+    }
+}
